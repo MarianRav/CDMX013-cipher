8d63e40364e3eb4cb1e4fc2dfe8128951a1f3515
Marian Villavicencio <marian.mlva.16@gmail.com>
2022-07-19 21:57:29 -0500

    Maquetado CSS y Inicio funcionalidad js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,43 @@
-const cipher = {
+/*const cipher = {
   // ...
 };
 
-export default cipher;
+export default cipher;*/
+
+
+/*Cifrado*/
+document.addEventListener("click",texto);
+
+function texto(){
+  let mensaje = document.getElementById('box').value;
+  let numero = document.getElementById("num").value; 
+
+  let codMes = mensaje.charCodeAt(0); /*Iterar el mensaje para que haga lo mismo con cada caracter"*/
+    console.log(codMes);
+    console.log(numero);
+  
+}
+
+function cifrado(){
+
+  let codMes = mensaje.charCodeAt(0); /*Iterar el mensaje para que haga lo mismo con cada caracter"*/
+  console.log(codMes);
+  
+}
+
+
+
+  
+
+
+/*Fórmula
+
+(x-65+n)%26+65*/
+  
+
+
+/* variables - recibir un número (input)
+   función--¿objeto?
+   iterar elementos
+   asignar valores diferentes dependiendo del input
+   regresar nuevos valores*/
