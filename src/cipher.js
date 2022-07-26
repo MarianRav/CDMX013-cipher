781527521c57a88c83b5e1a2f4d411ba9561f99d
Marian Villavicencio <marian.mlva.16@gmail.com>
2022-07-26 15:01:16 -0500

    Funciones con requerimientos del README

--- src/cipher.js
+++ src/cipher.js
@@ -1,54 +1,51 @@
-/*const cipher = {
-  // ...
+const cipher = {
+  
+  encode: function(offset, string){
+  
+    console.log(string);
+    console.log(typeof string);
+    console.log("Tu número secreto es " + offset);
+    console.log(typeof offset);
+    
+    let ejemplo = '';
+  
+    for(let i = 0; i < string.length; i++){
+      let caracter = string.charCodeAt([i]);
+      console.log("número ASCII " + caracter);
+      caracter= (caracter- 65 + offset)% 26 + 65; //FÓRMULA
+      console.log("el nuevo valor es " + caracter);
+      caracter = String.fromCharCode(caracter);
+      console.log(caracter);
+      ejemplo = ejemplo + caracter; 
+    }
+  
+    console.log("tu mensaje es: " + ejemplo);
+    alert("Tu mensaje secreto es: " + ejemplo);
+    
+  },
+
+  decode: function(offset, string){
+    console.log(string);
+    console.log(offset);
+  
+    let mensajeDecifrado= '';
+  
+    for(let i = 0; i < string.length; i++){
+      let caracter = string.charCodeAt([i]);
+      console.log("número ASCII " + string);
+      caracter= (caracter- 65 -offset)% 26 + 65; //FÓRMULA
+      caracter = String.fromCharCode(caracter);
+     
+      console.log(string);
+      mensajeDecifrado = mensajeDecifrado + string;
+    }
+    alert("Tu mensaje es: " + mensajeDecifrado);
+  }
 };
 
-export default cipher;*/
-
-
-/*Cifrado*/
-
-document.addEventListener("click",texto);
-
-function texto(){
-  let letras = document.getElementById('box').value;
-  let numero = parseInt(document.getElementById("num").value); 
-  let mensaje = letras.toUpperCase();
-
-  console.log(letras);
-  console.log(mensaje);
-  console.log(typeof mensaje);
-  console.log("Tu número secreto es " + numero);
-  console.log(typeof numero);
-  
-
-  for(let i = 0; i < mensaje.length; i++){
-    let caracter = mensaje.charCodeAt([i]);
-    console.log("número ASCII " + caracter);
-    caracter= (caracter- 65 + numero)% 26 + 65; //FÓRMULA
-    console.log("el nuevo valor es " + caracter);
-    caracter = String.fromCharCode(caracter);
-    console.log(caracter);
-    //alert("tu mensaje secreto es: " + (caracter[i]));
-  }
-  
-}
-
-/*function cifrar(caracter, numero){
-  let mensajeCifrado= (caracter[i] - 65 + numero)% 26 +65;
-  console.log(mensajeCifrado);
-}
-
-function cifrado(){
-
-  let codMes = mensaje.charCodeAt(0); 
-  console.log(codMes);
-  
-}*/
+export default cipher;
 
 
 
-/*Fórmula
-
-(x-65+n)%26+65*/
   
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+const cipherButton = document.getElementById("botonCifrado");
+console.log(cipherButton);
+
+cipherButton.addEventListener("click",function(){
+    let string = document.getElementById('box').value;
+    let offset = parseInt(document.getElementById("num").value); 
+    string = string.toUpperCase();
+    cipher.encode(offset, string)
+});
+
+//Preguntaa: Tengo dos pantallas y de acuerdo con el readme para los test, 
+//las variables deben tener el mismo nombre, pero aquí me va a dar error
+//debo hacer un archivo index. js para cada pantalla y no sean las mismas variables
+
+
+
+const decipherButton= document.getElementById("decipher");
+console.log(decipherButton);
+
+decipherButton.addEventListener("click", function(){
+    let string = document.getElementById('boxTwo').value;
+    let offset = parseInt(document.getElementById("number").value); 
+    string = string.toUpperCase();
+    cipher.decode(offset,string);
+});
