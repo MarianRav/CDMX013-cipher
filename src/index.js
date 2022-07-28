6cfe6e2e01a0fd55c95b22460d472d4b5d50d132
Marian Villavicencio <marian.mlva.16@gmail.com>
2022-07-28 16:54:05 -0500

    Caesar cipher finalizado.V.sencilla

--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ cipherButton.addEventListener("click",function(){
     let string = document.getElementById('box').value;
     let offset = parseInt(document.getElementById("num").value); 
     string = string.toUpperCase();
-    const respuestaCifrado = cipher.encode(offset, string)
+    const respuestaCifrado = cipher.encode(offset, string);
     document.getElementById("resultadoCifrado").style.display="block";
     document.getElementById("cifrado").style.display="none";
-    document.getElementById("popUp").innerHTML = respuestaCifrado
+    document.getElementById("popUp").innerHTML = respuestaCifrado;
+    document.getElementById("recordatorio").innerText = "Recuerda compartir el número secreto (" + offset + "), con quien quieres compartir tu mensaje";
 });
 
-//Preguntaa: Tengo dos pantallas y de acuerdo con el readme para los test, 
-//las variables deben tener el mismo nombre, pero aquí me va a dar error
-//debo hacer un archivo index. js para cada pantalla y no sean las mismas variables
+
 
